89c6bd0361f155eda549e2b104d252a9c06e52d9
natashaclerkin <natashaclerkin@gmail.com>
2020-11-18 18:33:00 +0000

    Fix js modal issue

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,12 +3,12 @@ $(document).ready(function () {
     $("a.nav-link").removeClass("active");
     $(this).addClass("active");
   });
-​
+
   $('.modal').on('hidden.bs.modal', function (e) {
     const url = $(e.currentTarget).data('videoUrl');
     $(e.currentTarget).find('iframe').attr('src', '');
   });
-​
+
   $('.modal').on('show.bs.modal', function (e) {
     const url = $(e.currentTarget).data('videoUrl');
     $(e.currentTarget).find('iframe').attr('src', url);
